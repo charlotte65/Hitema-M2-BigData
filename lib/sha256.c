ebbc31c23f72a511d41e3f42f92ca3169724953f
charlotte65 <105380601+charlotte65@users.noreply.github.com>
2020-07-22 14:54:24 +0000

    Bump version to 0.4.1

--- lib/sha256.c
+++ lib/sha256.c
@@ -1,3 +1,6 @@
 
 /*********************************************************************
 * Filename:   sha256.c
+* Author:     Brad Conte (brad AT bradconte.com)
+* Copyright:
+* Disclaimer: This code is presented "as is" without any guarantees.
