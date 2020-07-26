6baa02735451011f55b13bd69f7237501af76a35
charlotte65 <105380601+charlotte65@users.noreply.github.com>
2020-07-26 08:39:38 +0000

    modif

--- lib/sha256.c
+++ lib/sha256.c
@@ -4,3 +4,4 @@
 * Author:     Brad Conte (brad AT bradconte.com)
 * Copyright:
 * Disclaimer: This code is presented "as is" without any guarantees.
+* Details:    Implementation of the SHA-256 hashing algorithm.
