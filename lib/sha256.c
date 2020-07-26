20e3509cc36125276c9492907310f25c32f58c89
charlotte65 <105380601+charlotte65@users.noreply.github.com>
2020-07-26 10:43:22 +0000

    dev-java/netty-handler: Cleaning old version(s)

--- lib/sha256.c
+++ lib/sha256.c
@@ -5,3 +5,6 @@
 * Copyright:
 * Disclaimer: This code is presented "as is" without any guarantees.
 * Details:    Implementation of the SHA-256 hashing algorithm.
+              SHA-256 is one of the three algorithms in the SHA2
+              specification. The others, SHA-384 and SHA-512, are not
+              offered in this implementation.
