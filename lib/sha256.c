f64e1af16ae17d988fca2cb6d983caac72189f7e
charlotte65 <105380601+charlotte65@users.noreply.github.com>
2020-08-09 20:02:21 +0000

    AngularJS

--- lib/sha256.c
+++ lib/sha256.c
@@ -50,3 +50,6 @@ void sha256_transform(SHA256_CTX *ctx, const BYTE data[])
 		m[i] = (data[j] << 24) | (data[j + 1] << 16) | (data[j + 2] << 8) | (data[j + 3]);
 	for ( ; i < 64; ++i)
 		m[i] = SIG1(m[i - 2]) + m[i - 7] + SIG0(m[i - 15]) + m[i - 16];
+
+	a = ctx->state[0];
+	b = ctx->state[1];
