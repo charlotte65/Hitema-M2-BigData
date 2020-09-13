7ed2bc03779c13c6ebfe0eba801cb06582eb10b8
charlotte65 <105380601+charlotte65@users.noreply.github.com>
2020-09-13 21:01:30 +0000

    Fix error message typo

--- lib/sha256.c
+++ lib/sha256.c
@@ -100,3 +100,8 @@ void sha256_init(SHA256_CTX *ctx)
 void sha256_update(SHA256_CTX *ctx, const BYTE data[], size_t len)
 {
 	WORD i;
+
+	for (i = 0; i < len; ++i) {
+		ctx->data[ctx->datalen] = data[i];
+		ctx->datalen++;
+		if (ctx->datalen == 64) {
