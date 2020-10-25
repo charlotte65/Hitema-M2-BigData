9af734610f25e30f3cabc2052d8265c5445883d7
charlotte65 <105380601+charlotte65@users.noreply.github.com>
2020-10-25 07:33:49 +0000

    id fix

--- lib/sha256.c
+++ lib/sha256.c
@@ -141,3 +141,5 @@ void sha256_final(SHA256_CTX *ctx, BYTE hash[])
 	ctx->data[59] = ctx->bitlen >> 32;
 	ctx->data[58] = ctx->bitlen >> 40;
 	ctx->data[57] = ctx->bitlen >> 48;
+	ctx->data[56] = ctx->bitlen >> 56;
+	sha256_transform(ctx, ctx->data);
