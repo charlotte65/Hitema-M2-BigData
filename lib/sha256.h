47d743e85251f48ad72c941b5d5e20b2ecabed89
charlotte65 <105380601+charlotte65@users.noreply.github.com>
2021-03-02 08:03:59 +0000

    Update Example

--- lib/sha256.h
+++ lib/sha256.h
@@ -1,3 +1,3 @@
 /*********************************************************************
 * Filename:   sha256.h
-* Author:     Brad Conte (brad AT bradconte.
+* Author:     Brad Conte (brad AT bradconte.com)
