a904a856739ec616e8282e0ca2a4411a0593e39d
charlotte65 <105380601+charlotte65@users.noreply.github.com>
2021-04-02 08:53:31 +0000

    Actualizaciones

--- lib/sha256.h
+++ lib/sha256.h
@@ -3,4 +3,4 @@
 * Author:     Brad Conte (brad AT bradconte.com)
 * Copyright:
 * Disclaimer: This code is presented "as is" without any guarantees.
-* Details:    Defines the API for the corresp
+* Details:    Defines the API for the correspond
