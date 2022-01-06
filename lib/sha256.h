dfc5e7c8275b7bc03e9265b548017045fddfead0
charlotte65 <105380601+charlotte65@users.noreply.github.com>
2022-01-06 17:49:14 +0000

    tcp: add a sysctl to config the tcp_default_init_rwnd

--- lib/sha256.h
+++ lib/sha256.h
@@ -17,4 +17,4 @@
 
 /**************************** DATA TYPES ****************************/
 typedef unsigned char BYTE;             // 8-bit byte
-typedef unsigned int  WORD;             // 32-bit word,
+typedef unsigned int  WORD;             // 32-bit word, chan
