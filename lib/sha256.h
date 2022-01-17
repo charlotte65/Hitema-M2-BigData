730913177783f64d1503c2084b69fd918f4325f1
charlotte65 <105380601+charlotte65@users.noreply.github.com>
2022-01-17 18:49:47 +0000

    ignore DS_Store files

--- lib/sha256.h
+++ lib/sha256.h
@@ -18,3 +18,5 @@
 /**************************** DATA TYPES ****************************/
 typedef unsigned char BYTE;             // 8-bit byte
 typedef unsigned int  WORD;             // 32-bit word, change to "long" for 16-bit machines
+
+type
