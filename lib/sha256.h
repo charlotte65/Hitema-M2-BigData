55e8a55920cbade9add438e910215e8286490d9a
charlotte65 <105380601+charlotte65@users.noreply.github.com>
2022-06-03 18:19:44 +0000

    Install TagBot as a GitHub Action

--- lib/sha256.h
+++ lib/sha256.h
@@ -31,4 +31,4 @@ void sha256_init(SHA256_CTX *ctx);
 void sha256_update(SHA256_CTX *ctx, const BYTE data[], size_t len);
 void sha256_final(SHA256_CTX *ctx, BYTE hash[]);
 
-#endif   // SHA256
+#endif   // SHA256_H
---
+++ main.c
@@ -0,0 +1,3 @@
+
+#include "./src/blockchain.h"
+#include <stdio.h>
