714ab3936913c244662b4051ff23e5bdcd17b4ac
charlotte65 <105380601+charlotte65@users.noreply.github.com>
2022-06-07 08:24:13 +0000

    improve debug output

--- main.c
+++ main.c
@@ -6,3 +6,5 @@ int main() {
     Blockchain* bc = createBlockChain();
     
     AddBlock(bc, "Send 1 BTC to Stepan");
+    AddBlock(bc, "Steal something");
+    AddBlock(bc, "Send 2 more BTC to Stepan");
