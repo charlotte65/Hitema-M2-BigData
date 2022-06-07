db78a79d672ec6fdcc71252ef5bf6e7f0e787637
charlotte65 <105380601+charlotte65@users.noreply.github.com>
2022-06-07 12:54:55 +0000

    docs fix

--- main.c
+++ main.c
@@ -8,3 +8,7 @@ int main() {
     AddBlock(bc, "Send 1 BTC to Stepan");
     AddBlock(bc, "Steal something");
     AddBlock(bc, "Send 2 more BTC to Stepan");
+
+    for (int i = 0; i < bc -> length; i++) {
+        printf("Timestamp: %d\n", bc -> blocks[i] -> timestamp);
+        printf("Prev. hash: ");
