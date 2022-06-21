eea0c43f60481ca83dc6c300c345a04c92cc7152
charlotte65 <105380601+charlotte65@users.noreply.github.com>
2022-06-21 16:11:56 +0000

    Fixed capitalization

--- src/block.c
+++ src/block.c
@@ -14,3 +14,6 @@ BYTE* sha256(BYTE* data, size_t len) {
     SHA256_CTX myCtx;
     sha256_init(&myCtx);
     sha256_update(&myCtx, data, len);
+
+    BYTE* hash = malloc(sizeof(BYTE) * 32);
+    sha256_final(&myCtx, hash);
