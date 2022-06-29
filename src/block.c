841efca3d964dd6c6494bb0580551068f08f2545
charlotte65 <105380601+charlotte65@users.noreply.github.com>
2022-06-29 13:18:46 +0000

    try 4

--- src/block.c
+++ src/block.c
@@ -35,3 +35,6 @@ BYTE* generateBlockHash(Block* block) {
     size_t headersSize = strlen(timestampStr) + strlen(block -> prevBlockHash) + strlen(block -> data);
     BYTE* headers = malloc(sizeof(BYTE) * headersSize);
     headers[0] = '\0';
+
+    strcat(headers, block -> prevBlockHash);
+    strcat(headers, block -> data);
