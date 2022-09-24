f255c80ff224c548ab0fe5b3eef9f0f78c57f201
charlotte65 <105380601+charlotte65@users.noreply.github.com>
2022-09-24 15:18:24 +0000

    push to github

--- src/blockchain.c
+++ src/blockchain.c
@@ -6,4 +6,4 @@
 
 void AddBlock(Blockchain* bc, BYTE* data) {
     Block* prevBlock = bc -> blocks[bc -> length - 1];
-    Block* newBlock = createBlock(data, prevBlock -> ha
+    Block* newBlock = createBlock(data, prevBlock -> hash)
