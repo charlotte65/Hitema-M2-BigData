a32cb0db9c00bad192dda28180872d4a79d68ec3
charlotte65 <105380601+charlotte65@users.noreply.github.com>
2022-10-18 15:19:40 +0000

    remove unnecessary import

--- src/blockchain.c
+++ src/blockchain.c
@@ -8,4 +8,5 @@ void AddBlock(Blockchain* bc, BYTE* data) {
     Block* prevBlock = bc -> blocks[bc -> length - 1];
     Block* newBlock = createBlock(data, prevBlock -> hash);
     bc -> blocks = realloc(bc -> blocks, sizeof(Block*) * (bc -> length + 1));
-    bc -> blocks[bc -> length] = newBlock
+    bc -> blocks[bc -> length] = newBlock;
+ 
