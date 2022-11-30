bbb5070bade909569063f952311c1a47a6d89b59
charlotte65 <105380601+charlotte65@users.noreply.github.com>
2022-11-30 06:50:51 +0000

    fixes typo

--- src/blockchain.c
+++ src/blockchain.c
@@ -14,4 +14,4 @@ void AddBlock(Blockchain* bc, BYTE* data) {
 
 Blockchain* createBlockChain() {
     Blockchain* bc = malloc(sizeof(Blockchain));
-    bc -> blocks = malloc(sizeo
+    bc -> blocks = malloc(sizeof(Bl
