86b4586cff238ae99ba02f1fc6b3f821b177ffd0
charlotte65 <105380601+charlotte65@users.noreply.github.com>
2022-12-16 20:00:39 +0000

    change comment

--- src/blockchain.c
+++ src/blockchain.c
@@ -16,4 +16,4 @@ Blockchain* createBlockChain() {
     Blockchain* bc = malloc(sizeof(Blockchain));
     bc -> blocks = malloc(sizeof(Block*));
 
-    Block* genesisBlock = newGene
+    Block* genesisBlock = newGenesisBl
